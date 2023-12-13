32a5f75c3410750228e516543eaeb13600f9d1b4
yzx9 <yuan.zx@outlook.com>
2023-12-13 15:53:19 +0800

    Update README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+My personal website.
+
+Deploy: https://yzx9.github.io
+
+This is a replicas, see main repo: https://github.com/yzx9/blog
--- assets/chunks/theme.ieC1e_b3.js
+++
@@ -1,1 +0,0 @@
-import{r as a,i as h}from"./framework.heUNgYAJ.js";const o=a(h?location.hash:"");h&&window.addEventListener("hashchange",()=>{o.value=location.hash});a();
